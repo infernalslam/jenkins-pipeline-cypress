e910557e643aa3174ec2e93ad5c4e216ef105dd8
infernalslam <infernal-slam@hotmail.com>
2019-02-25 00:27:08 +0700

    fix

--- cypress/integration/example/test.spec.js
+++ cypress/integration/example/test.spec.js
@@ -2,4 +2,8 @@ describe('Test deploy jenkins pipeline', function() {
   it('successfully jenkins build pipeline', function() {
     cy.expect(true).to.equal(true)
   })
+  it('faile jenkins build pipeline', function() {
+    cy.visit('https://www.google.com/')
+    cy.url().should('match', /login/)
+  })
 })
